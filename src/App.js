66f478df797574937dc76b61210d238ba06c2c0b
Javidan Gurbanli <cqurbanli1@std.beu.edu.az>
2025-07-17 11:34:40 +0400

    fixed loading stuck

--- src/App.js
+++ src/App.js
@@ -15,61 +15,49 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Checkout from "./pages/Checkout/Checkout";
 import Terms from "./pages/Terms/Terms";
-import './i18n';
-import { Suspense, useEffect, useState } from "react";
+import "./i18n";
+import { Suspense } from "react";
 import Loading from "./components/Loading/Loading";
 import Faq from "./pages/FAQ/Faq";
 import Contact from "./pages/Contact/Contact";
 import About from "./pages/About/About";
-import { Helmet } from 'react-helmet';
+import { Helmet } from "react-helmet";
 
 function App() {
-const [isLoading, setIsLoading] = useState(true);
-const handleLoading = () => {
-setIsLoading(false);
-}
-useEffect(()=>{
-window.addEventListener("load",handleLoading);
-return () => window.removeEventListener("load",handleLoading);
-},[])
-
   return (
-  <Suspense fallback={<Loading/>}>
-    <ChakraProvider>
-      <CartProvider> 
-      <Helmet>
-          <title>RoFruit</title>
-        </Helmet>
-        {isLoading ? (
-          <div className="loading">
-            <Loading/>
-          </div>
-        ):(
+    <Suspense fallback={<Loading />}>
+      <ChakraProvider>
+        <CartProvider>
+          <Helmet>
+            <title>RoFruit</title>
+          </Helmet>
           <div className="App">
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products/:category" element={<Products />} />
-            <Route path="/favourites" element={<Favourites />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/basket" element={<Basket />} />
-            <Route path="/products/:category/:id" element={<ProductDetails />}/>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/terms" element={<Terms/>}/>
-            <Route path="/questions" element={<Faq/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="*" element={<Navigate to="/404" />} />
-            <Route path="/404" element={<NotFound />} />
-          </Routes>
-          <Footer />
-        </div>
-        )}
-      </CartProvider>
-    </ChakraProvider>
-  </Suspense>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products/:category" element={<Products />} />
+              <Route path="/favourites" element={<Favourites />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/basket" element={<Basket />} />
+              <Route
+                path="/products/:category/:id"
+                element={<ProductDetails />}
+              />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/terms" element={<Terms />} />
+              <Route path="/questions" element={<Faq />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about" element={<About />} />
+              <Route path="*" element={<Navigate to="/404" />} />
+              <Route path="/404" element={<NotFound />} />
+            </Routes>
+            <Footer />
+          </div>
+        </CartProvider>
+      </ChakraProvider>
+    </Suspense>
   );
 }
 
